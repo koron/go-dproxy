4c7ae51278777cb623f198f584996ef6eb9673a3
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-03-31 13:21:57 +0900

    brush up drain's I/F

--- drain.go
+++ drain.go
@@ -8,112 +8,124 @@ type Drain struct {
 }
 
 // Has returns true if the drain stored some of errors.
-func (s *Drain) Has() bool {
-	return s != nil && s.errors != nil
+func (d *Drain) Has() bool {
+	return d != nil && d.errors != nil
 }
 
 // First returns a stored error.  Returns nil if there are no errors.
-func (s *Drain) First() error {
-	if s == nil || s.errors == nil {
+func (d *Drain) First() error {
+	if d == nil || d.errors == nil {
 		return nil
 	}
-	return s.errors[0]
+	return d.errors[0]
 }
 
 // All returns all errors which stored.  Return nil if no errors stored.
-func (s *Drain) All() []error {
-	if s == nil || s.errors == nil {
+func (d *Drain) All() []error {
+	if d == nil || d.errors == nil {
 		return nil
 	}
-	a := make([]error, 0, len(s.errors))
-	return append(a, s.errors...)
+	a := make([]error, 0, len(d.errors))
+	return append(a, d.errors...)
 }
 
 // CombineErrors returns an error which combined all stored errors.  Return nil
 // if not erros stored.
-func (s *Drain) CombineErrors() error {
-	if s == nil || s.errors == nil {
+func (d *Drain) CombineErrors() error {
+	if d == nil || d.errors == nil {
 		return nil
 	}
-	return drainError(s.errors)
+	return drainError(d.errors)
 }
 
-func (s *Drain) put(err error) {
+func (d *Drain) put(err error) {
 	if err == nil {
 		return
 	}
-	s.errors = append(s.errors, err)
+	d.errors = append(d.errors, err)
 }
 
 // Bool returns bool value and stores an error.
-func (s *Drain) Bool(v bool, err error) bool {
-	s.put(err)
+func (d *Drain) Bool(p Proxy) bool {
+	v, err := p.Bool()
+	d.put(err)
 	return v
 }
 
 // Int64 returns int64 value and stores an error.
-func (s *Drain) Int64(v int64, err error) int64 {
-	s.put(err)
+func (d *Drain) Int64(p Proxy) int64 {
+	v, err := p.Int64()
+	d.put(err)
 	return v
 }
 
 // Float64 returns float64 value and stores an error.
-func (s *Drain) Float64(v float64, err error) float64 {
-	s.put(err)
+func (d *Drain) Float64(p Proxy) float64 {
+	v, err := p.Float64()
+	d.put(err)
 	return v
 }
 
 // String returns string value and stores an error.
-func (s *Drain) String(v string, err error) string {
-	s.put(err)
+func (d *Drain) String(p Proxy) string {
+	v, err := p.String()
+	d.put(err)
 	return v
 }
 
 // Array returns []interface{} value and stores an error.
-func (s *Drain) Array(v []interface{}, err error) []interface{} {
-	s.put(err)
+func (d *Drain) Array(p Proxy) []interface{} {
+	v, err := p.Array()
+	d.put(err)
 	return v
 }
 
 // Map returns map[string]interface{} value and stores an error.
-func (s *Drain) Map(v map[string]interface{}, err error) map[string]interface{} {
-	s.put(err)
+func (d *Drain) Map(p Proxy) map[string]interface{} {
+	v, err := p.Map()
+	d.put(err)
 	return v
 }
 
 // BoolArray returns []bool value and stores an error.
-func (s *Drain) BoolArray(v []bool, err error) []bool {
-	s.put(err)
+func (d *Drain) BoolArray(ps ProxySet) []bool {
+	v, err := ps.BoolArray()
+	d.put(err)
 	return v
 }
 
 // Int64Array returns []int64 value and stores an error.
-func (s *Drain) Int64Array(v []int64, err error) []int64 {
-	s.put(err)
+func (d *Drain) Int64Array(ps ProxySet) []int64 {
+	v, err := ps.Int64Array()
+	d.put(err)
 	return v
 }
 
 // Float64Array returns []float64 value and stores an error.
-func (s *Drain) Float64Array(v []float64, err error) []float64 {
-	s.put(err)
+func (d *Drain) Float64Array(ps ProxySet) []float64 {
+	v, err := ps.Float64Array()
+	d.put(err)
 	return v
 }
 
 // StringArray returns []string value and stores an error.
-func (s *Drain) StringArray(v []string, err error) []string {
-	s.put(err)
+func (d *Drain) StringArray(ps ProxySet) []string {
+	v, err := ps.StringArray()
+	d.put(err)
 	return v
 }
 
 // ArrayArray returns [][]interface{} value and stores an error.
-func (s *Drain) ArrayArray(v [][]interface{}, err error) [][]interface{} {
-	s.put(err)
+func (d *Drain) ArrayArray(ps ProxySet) [][]interface{} {
+	v, err := ps.ArrayArray()
+	d.put(err)
 	return v
 }
 
 // MapArray returns []map[string]interface{} value and stores an error.
-func (s *Drain) MapArray(v []map[string]interface{}, err error) []map[string]interface{} {
-	s.put(err)
+func (d *Drain) MapArray(ps ProxySet) []map[string]interface{} {
+	v, err := ps.MapArray()
+	d.put(err)
 	return v
 }
 
--- drain_test.go
+++ drain_test.go
@@ -12,21 +12,21 @@ func TestDrainBool(t *testing.T) {
 
 	d := new(Drain)
 
-	foo := d.Bool(New(v).M("foo").Bool())
+	foo := d.Bool(New(v).M("foo"))
 	if d.Has() {
 		t.Error(d.First())
 	} else if foo != true {
 		t.Errorf("foo must be true")
 	}
 
-	bar := d.Bool(New(v).M("bar").Bool())
+	bar := d.Bool(New(v).M("bar"))
 	if d.Has() {
 		t.Error(d.First())
 	} else if bar != false {
 		t.Errorf("bar must be false")
 	}
 
-	baz := d.Bool(New(v).M("baz").Bool())
+	baz := d.Bool(New(v).M("baz"))
 	if !d.Has() {
 		t.Error("baz must not exist")
 	} else if err := d.First(); err == nil || err.Error() != "not found: baz" {
